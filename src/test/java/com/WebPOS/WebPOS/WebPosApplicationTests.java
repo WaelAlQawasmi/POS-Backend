636b33045cbd8a72c810c3f2efb936cf15650510
waelalq <wael.abuomr@gmail.com>
2024-01-25 07:01:13 +0200

    first commit

---
+++ src/test/java/com/WebPOS/WebPOS/WebPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebPOS.WebPOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
