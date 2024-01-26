ad59f78345736deefb4687b8af436a4a13fa093e
waelalq <wael.abuomr@gmail.com>
2024-01-26 11:52:45 +0200

    auth

--- src/main/java/com/WebPOS/WebPOS/Entities/User.java
+++ src/main/java/com/WebPOS/WebPOS/Entities/User.java
@@ -2,8 +2,15 @@ package com.WebPOS.WebPOS.Entities;
 
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Data
 @NoArgsConstructor
@@ -13,7 +20,7 @@ import org.springframework.data.annotation.Id;
     uniqueConstraints = {
             @UniqueConstraint(columnNames = "email")
     })
-public class User {
+public class User implements UserDetails {
     @jakarta.persistence.Id
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,4 +36,33 @@ public class User {
     private String password;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
---
+++ src/main/java/com/WebPOS/WebPOS/Security/userDeletesService.java
@@ -0,0 +1,20 @@
+package com.WebPOS.WebPOS.Security;
+
+import com.WebPOS.WebPOS.Entities.User;
+import com.WebPOS.WebPOS.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class userDeletesService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return user;
+    }
+}
